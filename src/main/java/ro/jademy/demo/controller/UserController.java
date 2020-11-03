ee2212cec84cb2f702d309b427b92ba9c3578236
Catalin Tudorache <catalin.tudorache87@gmail.com>
2020-11-03 22:02:44 +0200

    Added user details page.

--- src/main/java/ro/jademy/demo/controller/UserController.java
+++ src/main/java/ro/jademy/demo/controller/UserController.java
@@ -3,24 +3,44 @@ package ro.jademy.demo.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import ro.jademy.demo.exception.UserNotFoundException;
 import ro.jademy.demo.model.User;
 
 import java.util.Arrays;
+import java.util.List;
+import java.util.Optional;
 
 @Controller
 public class UserController {
 
     @GetMapping("/users")
     public String getUsers(Model model) {
-
-        User user1 = new User("Ion", "Ionescu", "ion@example.com");
-        User user2 = new User("Gigi", "Gigescu", "gigi@example.com");
-        User user3 = new User("Marinescu", "Marinescu", "maria@example.com");
-
-        model.addAttribute("users", Arrays.asList(user1, user2, user3));
+        model.addAttribute("users", getUsers());
 
         return "users"; // searches for a users.html template page in resources/templates
     }
 
+    @GetMapping("/users/{id}")
+    public String getUserDetails(@PathVariable("id") Long id, Model model) {
 
+        Optional<User> userOpt = getUsers().stream().filter(user -> user.getId().equals(id)).findFirst();
+
+        if (userOpt.isPresent()) {
+            model.addAttribute("user", userOpt.get());
+
+            return "usersDetails";
+        } else {
+            // should throw 404
+            throw new UserNotFoundException("User with ID " + id + " not found!");
+        }
+    }
+
+    private static List<User> getUsers() {
+        User user1 = new User(1L, "Ion", "Ionescu", "ion@example.com", "123456789", "Str. 1");
+        User user2 = new User(2L, "Gigi", "Gigescu", "gigi@example.com", "9876543213", "Str. 2");
+        User user3 = new User(3L, "Maria", "Marinescu", "maria@example.com", "2345675432", "Str. 3");
+
+        return Arrays.asList(user1, user2, user3);
+    }
 }
--- src/main/java/ro/jademy/demo/model/User.java
+++ src/main/java/ro/jademy/demo/model/User.java
@@ -9,7 +9,10 @@ import lombok.NoArgsConstructor;
 @Data
 public class User {
 
+    private Long id;
     private String firstName;
     private String lastName;
     private String email;
+    private String phoneNumber;
+    private String address;
 }
